498e623aaef9409fe6ab5e60875416e7b560ca22
Grzegorz Matuszak <geniesis@o2.pl>
2020-10-31 12:20:54 +0100

    Added removed ()

--- app/src/services/viewExtension/mainView/MainViewHome.py
+++ app/src/services/viewExtension/mainView/MainViewHome.py
@@ -8,5 +8,5 @@ class MainViewHome:
         trainingQuery = TrainingQuery()
 
         view.main_training_today.setText(str(trainingQuery.findTodayTrainingQuantity()))
-        view.main_training_this_week.setText(str(trainingQuery.findWeekTrainingQuantity))
+        view.main_training_this_week.setText(str(trainingQuery.findWeekTrainingQuantity()))
         view.main_training_this_month.setText(str(trainingQuery.findMonthTrainingQuantity()))
