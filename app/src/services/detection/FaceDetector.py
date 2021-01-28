0471f5e9491bdd0071530d465e145fbdbe141356
konnyz <konradnyznyk@o2.pl>
2021-01-28 20:07:35 +0100

    dodano licznik cwiczen i licznik biernosci

--- app/src/services/detection/FaceDetector.py
+++ app/src/services/detection/FaceDetector.py
@@ -17,11 +17,7 @@ class FaceDetector():
 
         # Captures the stream from laptop camera
         video_capture = cv2.VideoCapture(0)
-
-        check_length = 10
-        start_time = time.time()
-
-        while True:
+ while True:
             # Capture frame-by-frame
             ret, frame = video_capture.read()
             # Colours the image grey, so that it can be recognized by opencv
@@ -36,11 +32,47 @@ class FaceDetector():
 
             if type(faces) == tuple:
                 print("User not detected")
+                return False
             elif type(faces) == numpy.ndarray:
                 print("User detected")
+                return True
 
-            current_time = time.time()
-            if int(current_time - start_time) > check_length:
-                break
 
             time.sleep(1)
+
+    
+    def time_difference(check_length):
+        start_time = time.time()
+        sound = "dzwonek.mp3"
+        while True:
+            if FaceDetect.isUserDetected():
+                start_time = time.time()
+            else:
+                current_time = time.time()
+                time_difference = int(current_time - start_time)
+                print(time_difference)
+                if time_difference >= check_length:
+                    print("Brawo! Udało Ci się wykonać ćwiczenie!")
+                    playsound(sound)
+                    break
+    def check_user(check_length):
+        lista = []
+        for i in range(5):
+            lista.append(FaceDetect.isUserDetected())
+        if sum(lista) == 0:
+            print("rozpoczynam sprawdzanie")
+            start_time = time.time()
+            sound = "dzwonek.mp3"
+            while True:
+                if FaceDetect.isUserDetected():
+                    break
+                else:
+                    current_time = time.time()
+                    time_difference = int(current_time - start_time)
+                    print(time_difference)
+                    if time_difference >= check_length:
+                        print("czas do kolejnego ćwiczenia został zresetowany")
+                        playsound(sound)
+                        #tu resetujemy glowny timer
+                        break
+FaceDetect.check_user(5)
